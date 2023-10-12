003d9c8cc606ff5e4369322f2f0fb2823d13a243
linchuan33 <linchuanchen@oakland.edu>
2023-10-12 16:19:58 -0400

    HW5 upload

--- MetricConverterApp_HW5.java
+++ MetricConverterApp_HW5.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class MetricConverterApp {
+public class MetricConverterApp_HW5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+//开头给予选项
         System.out.println("Welcome to Metric Converter!");
-        System.out.println("Please input your query. For example, 1 km = m.");
+        System.out.println("Please input your query. For example, 1 km = mi.");
         System.out.println("Enter 'exit' or '-1' to exit the program.");
-
+//in put检测
         while (true) {
             System.out.print("Enter your conversion query: ");
             String input = scanner.nextLine();
@@ -48,9 +48,9 @@ public class MetricConverterApp {
             System.out.println("Conversion not supported or invalid units.");
         }
     }
-
+//计算方式
     private static double convert(double value, String unitFrom, String unitTo) {
-        // Implement your conversion logic here for supported conversions.
+        
         if (unitFrom.equals("kg") && unitTo.equals("lb")) {
             return value * 2.20462;
         } else if (unitFrom.equals("g") && unitTo.equals("oz")) {
@@ -60,6 +60,6 @@ public class MetricConverterApp {
         } else if (unitFrom.equals("mm") && unitTo.equals("in")) {
             return value * 0.0393701;
         }
-        return -1; // Unsupported conversion or invalid units.
+        return -1; 
     }
 }
