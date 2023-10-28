23904f7fb195ba3bb1db322a2cc014f6f13cc2d8
linchuan33 <linchuanchen@oakland.edu>
2023-10-27 20:44:22 -0400

    ASS8 upload

---
+++ Assignment8:Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Assignment8:Fibonacci {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        System.out.println("Fibonacci number at position " + n + " is: " + fibonacci(n));
+    }
+}
